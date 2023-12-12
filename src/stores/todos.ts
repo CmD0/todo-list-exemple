280761111f49da7e35633150099826be896cd655
CmD0 <elouan112@gmail.com>
2023-12-12 13:22:10 -0500

    made the store persistent

--- src/stores/todos.ts
+++ src/stores/todos.ts
@@ -2,9 +2,10 @@ import {ref} from 'vue'
 import {defineStore} from 'pinia'
 
 export const useTodoStore = defineStore('counter', () => {
-    const todos = ref<{text:string, completed:boolean}[]>([])
+    const todos = ref<{text:string, completed:boolean}[]>(localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')!) : [])
     function addTodo(todo: {text:string, completed:boolean}) {
         todos.value.push(todo)
+        localStorage.setItem('todos', JSON.stringify(todos.value))
     }
     return {todos, addTodo}
 })
